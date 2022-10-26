659cf9e34c1c8cec06e70bb88bb8e95d80f3b10b
bakasub <flannoblesse@gmail.com>
2022-10-26 10:21:32 +0700

    routing

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const http = require('http')
+const url = require('url')
+const handler = require('./controller/router')
+const notFoundRouting = require('./controller/handle/notFoundRouting')
+
+function getUrl(req) {
+    const urlParse = url.parse(req.url,true)
+    const pathName = urlParse.pathname
+    return pathName.split('/')
+}
 
 const server = http.createServer((req,res)=>{
-
+    const arrPath = getUrl(req)
+    let trimPath = ''
+    if (arrPath.length > 2 ){
+        trimPath = arrPath[1] + '/' + arrPath[2]
+    } else {
+        trimPath = arrPath[arrPath.length - 1]
+    }
+    let chosenHandle
+    if (typeof handler[trimPath] === 'undefined' ){
+        chosenHandle = notFoundRouting.showNotFound
+    } else {
+        chosenHandle = handler[trimPath]
+    }
+     chosenHandle(req,res)
 })
 
 server.listen(8080,()=>{
